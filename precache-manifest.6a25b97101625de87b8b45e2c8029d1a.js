1f52f4a3316245d7d1542e0f09a08f9a5b7ff152
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-07-08 21:26:07 -0700

    Updates

---
+++ precache-manifest.6a25b97101625de87b8b45e2c8029d1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e40cebbd1d6e3dbab92e483215067c",
+    "url": "/samesies/index.html"
+  },
+  {
+    "revision": "d22bc9af973b317a1613",
+    "url": "/samesies/static/css/main.9898e299.chunk.css"
+  },
+  {
+    "revision": "cbf27d5cee30e18a8268",
+    "url": "/samesies/static/js/2.945c1068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/samesies/static/js/2.945c1068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22bc9af973b317a1613",
+    "url": "/samesies/static/js/main.5dc3eae1.chunk.js"
+  },
+  {
+    "revision": "7ca707e4b55ffe9ed9f2",
+    "url": "/samesies/static/js/runtime-main.74066a44.js"
+  }
+]);
