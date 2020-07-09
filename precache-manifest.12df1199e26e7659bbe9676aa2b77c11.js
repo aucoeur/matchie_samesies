423beb9758bae520cfae867c0553e19b40e6d5bc
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-07-08 21:43:29 -0700

    Updates

--- precache-manifest.12df1199e26e7659bbe9676aa2b77c11.js
+++ precache-manifest.12df1199e26e7659bbe9676aa2b77c11.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c8e40cebbd1d6e3dbab92e483215067c",
+    "revision": "07a5c787476db6283cca4886dc6cb676",
     "url": "/samesies/index.html"
   },
   {
-    "revision": "d22bc9af973b317a1613",
+    "revision": "e3d08683a32084211e27",
     "url": "/samesies/static/css/main.9898e299.chunk.css"
   },
   {
-    "revision": "cbf27d5cee30e18a8268",
-    "url": "/samesies/static/js/2.945c1068.chunk.js"
+    "revision": "45dca4a0db5669b1e22d",
+    "url": "/samesies/static/js/2.4db814c4.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/samesies/static/js/2.945c1068.chunk.js.LICENSE.txt"
+    "url": "/samesies/static/js/2.4db814c4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d22bc9af973b317a1613",
-    "url": "/samesies/static/js/main.5dc3eae1.chunk.js"
+    "revision": "e3d08683a32084211e27",
+    "url": "/samesies/static/js/main.db85871d.chunk.js"
   },
   {
     "revision": "7ca707e4b55ffe9ed9f2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/samesies/precache-manifest.6a25b97101625de87b8b45e2c8029d1a.js"
+  "/samesies/precache-manifest.12df1199e26e7659bbe9676aa2b77c11.js"
 );
 
 self.addEventListener('message', (event) => {
