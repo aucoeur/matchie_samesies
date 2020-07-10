75167614399b69f01680ec2c5976e73fea92f433
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-07-10 15:23:11 -0700

    Updates

---
+++ precache-manifest.292809d3177d712c4115f40bed476bf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884df7730dc459d120e6f119b33f1bed",
+    "url": "/samesies/index.html"
+  },
+  {
+    "revision": "ba38e5b4dc1323b337be",
+    "url": "/samesies/static/css/main.430f866b.chunk.css"
+  },
+  {
+    "revision": "a4fc8f96bd78ea72199d",
+    "url": "/samesies/static/js/2.2a1fe9c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/samesies/static/js/2.2a1fe9c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba38e5b4dc1323b337be",
+    "url": "/samesies/static/js/main.c67222a6.chunk.js"
+  },
+  {
+    "revision": "7ca707e4b55ffe9ed9f2",
+    "url": "/samesies/static/js/runtime-main.74066a44.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/samesies/precache-manifest.c2001ed29ee25b5a6d2647f21b4027cb.js"
+  "/samesies/precache-manifest.292809d3177d712c4115f40bed476bf3.js"
 );
 
 self.addEventListener('message', (event) => {
